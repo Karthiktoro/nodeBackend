343a290b40faee86b1e2c07b6478730453d5a5f3
karthik <karthvc@gmail.com>
2024-06-27 14:40:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Dummy data
+let items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+    { id: 4, name: 'Item 4' },
+    { id: 10, name: 'Item 5' },
+];
+
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+app.post('/items', (req, res) => {
+    const newItem = {
+        id: items.length + 1,
+        name: req.body.name,
+    };
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
